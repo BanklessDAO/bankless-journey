de915ef83f2a9a97a7a7fcca5e11d3d0cc1b7392
Didier Krux <didier@krux.co>
2022-01-13 20:42:34 +0100

    force redirect to /bankless

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { VStack, Heading, Box, LinkOverlay, LinkBox } from "@chakra-ui/layout"
 import { Text } from '@chakra-ui/react'
 import { GetStaticProps } from "next"
-import React from "react"
+import React, { useEffect } from "react"
 import NextLink from "next/link"
+import Router from "next/router"
 import { getJourneys } from "services/journey"
 import { Journey } from "types"
 import { DEFAULT_REVALIDATE_PERIOD, DESCRIPTION } from "utils/constants"
@@ -12,32 +13,13 @@ interface Props {
 }
 
 export default function HomePage(props: Props) {
-  return <div>
-    <Box mb={4}>
-      <p>{DESCRIPTION}</p>
-    </Box>
-
-    <VStack as='section'
-      spacing={4}
-      align="stretch">
-      <Heading as="h3" size='lg'>Explore</Heading>
-
-      {props.journeys.map((journey: Journey) => {
-        return (
-          <LinkBox key={journey.id} as='article' my={4} p={4} borderWidth="1px" borderRadius="lg" overflow="hidden">
-            <Heading fontSize='xl'>
-              <NextLink href={`/${journey.id}`} passHref>
-                <LinkOverlay>
-                  {journey.name}
-                </LinkOverlay>
-              </NextLink>
-            </Heading>
-            <Text mt={4}>{journey.description}</Text>
-          </LinkBox>
-        )})
-      }
-    </VStack>
-  </div>
+  useEffect(() => {
+    const { pathname } = Router
+    if (pathname == "/") {
+      Router.push("/bankless")
+    }
+  })
+  return null
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
